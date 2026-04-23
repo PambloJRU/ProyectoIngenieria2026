3b46a575f9aa6530946a2aaedeb68baa6f2c3584
anye03 <anyelith.suarez@ucr.ac.cr>
2026-04-23 09:23:50 -0600

    Envio de datos procesados al backend

--- Proyecto-Kotlin/app/src/main/java/com/gruposinpe/proyectosinpe_kotlin/network/ApiService.kt
+++ Proyecto-Kotlin/app/src/main/java/com/gruposinpe/proyectosinpe_kotlin/network/ApiService.kt
@@ -1,13 +1,20 @@
 package com.gruposinpe.proyectosinpe_kotlin.network
-import com.android.volley.Response
 import com.gruposinpe.proyectosinpe_kotlin.model.SmsRequest
+import com.gruposinpe.proyectosinpe_kotlin.model.SinpePayment
 import okhttp3.ResponseBody
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.Response
 
 // TODO: Implementar cliente HTTP para enviar pagos al backend .NET
 
 interface ApiService {
+
     @POST("api/Sms/receive")
     suspend fun enviarSmsAlServidor(@Body sms: SmsRequest): Response<ResponseBody>
+
+    @POST("api/Sinpe/receive")
+    suspend fun enviarPagoProcesado(@Body payment: SinpePayment): Response<ResponseBody>
+
+
 }
