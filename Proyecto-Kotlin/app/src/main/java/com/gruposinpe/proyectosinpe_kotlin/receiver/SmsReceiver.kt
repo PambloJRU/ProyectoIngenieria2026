54ce2b6d6a84a10c0cf459b942512fdc40461d35
Joshua <joshua.cespedesgomez@ucr.ac.cr>
2026-04-24 23:02:13 -0600

    configuracion en el manifest para recibir sms

--- Proyecto-Kotlin/app/src/main/java/com/gruposinpe/proyectosinpe_kotlin/receiver/SmsReceiver.kt
+++ Proyecto-Kotlin/app/src/main/java/com/gruposinpe/proyectosinpe_kotlin/receiver/SmsReceiver.kt
@@ -1,5 +1,13 @@
 package com.gruposinpe.proyectosinpe_kotlin.receiver
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 
+class SmsReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
+}
 
 // TODO: Implementar BroadcastReceiver para escuchar SMS de SINPE Móvil
