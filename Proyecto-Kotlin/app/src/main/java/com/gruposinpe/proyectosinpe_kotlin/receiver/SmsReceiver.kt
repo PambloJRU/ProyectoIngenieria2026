e0eb7383cec0e4967689f112a78e7404f622b477
Joshua <joshua.cespedesgomez@ucr.ac.cr>
2026-04-27 10:44:12 -0600

    implementacion en el smsreceiver y cambios en el manisfest

--- Proyecto-Kotlin/app/src/main/java/com/gruposinpe/proyectosinpe_kotlin/receiver/SmsReceiver.kt
+++ Proyecto-Kotlin/app/src/main/java/com/gruposinpe/proyectosinpe_kotlin/receiver/SmsReceiver.kt
@@ -3,10 +3,45 @@ package com.gruposinpe.proyectosinpe_kotlin.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.telephony.SmsMessage
+import android.util.Log
+import android.widget.Toast
 
 class SmsReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
 
+    companion object{
+        private const val TAG = "SmsReceiver"
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action == "android.provider.Telephony.SMS_RECEIVED"){
+            val bundle: Bundle? = intent.extras
+
+            if(bundle != null){
+                try{
+                    val pdus = bundle.get("pdus") as Array<*>
+
+                    for(pdu in pdus){
+                        val smsMessage: SmsMessage = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                            val format = bundle.getString("format")
+                            SmsMessage.createFromPdu(pdu as ByteArray, format)
+                        }else{
+                            SmsMessage.createFromPdu(pdu as ByteArray)
+                        }
+
+                        Log.d(TAG, "Mensaje recibido")
+
+                        if (context != null) {
+                            Toast.makeText(context, "SMS Recibido", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } catch(e: Exception){
+                    Log.e(TAG, "Error: ${e.message}")
+                }
+            }
+        }
     }
 }
 
